f0a64708610b3b61d93cd5c8ead041a8c74e0fdc
Anton Kanugalawattage <antondilon@gmail.com>
2020-01-26 06:49:08 -0500

    spacing

--- src/Components/PopularItems.jsx
+++ src/Components/PopularItems.jsx
@@ -10,13 +10,13 @@ export class PopularItems extends Component {
         var tutors = this.props.popCourse
         var subjects = this.props.popSubject
         return (
-            <div className="col-lg-10" style={{margin:'auto', marginTop:'4%'}}>
+            <div className="col-lg-10" style={{margin:'auto', marginTop:'4%', marginBottom:'3%'}}>
                 <div className="row" >
                 <div className="col-lg-6" style={{margin:'auto', paddingLeft:'0px', paddingRight:'30px'}}>
                         <h6 style={{ fontFamily:'Avenir, sans-serif', fontWeight:'900', fontSize:'30px'}}>Popular Tutors</h6>
                         <div className="row">
                         {tutors.map((tutor, index) =>
-                            <div className="col-lg-4" style={{textAlign:'center', marginBottom:'30px', cursor:'pointer', padding:'10px'}} onClick={()=>{this.props.update(tutor.courseName)}}>
+                            <div className="col-lg-4" style={{textAlign:'center',  cursor:'pointer', padding:'10px'}} onClick={()=>{this.props.update(tutor.courseName)}}>
                                 <div style={{height:'250px'}}>
                                     <img className="resultCardImg" src={tutor.image} style={{height:'250px' ,paddingBottom:'0px',objectFit: 'cover', opacity:'1',background:"rgba(0,0,0,1)", borderRadius:'15px'}} alt=""></img>
                                 </div>
@@ -31,7 +31,7 @@ export class PopularItems extends Component {
                         <h6 style={{ fontFamily:'Avenir, sans-serif', fontWeight:'900', fontSize:'30px'}}>Popular Subjects</h6>
                         <div className="row">
                         {subjects.map((subject, index) =>
-                            <div className="col-lg-4" style={{textAlign:'center', marginBottom:'30px', cursor:'pointer', padding:'10px'}} onClick={(input)=>{this.setState({input}); this.props.update(subject.subject)}}>
+                            <div className="col-lg-4" style={{textAlign:'center',  cursor:'pointer', padding:'10px'}} onClick={(input)=>{this.setState({input}); this.props.update(subject.subject)}}>
                                 <div style={{height:'250px'}}>
                                     <img className="resultCardImg" src={subject.asset} style={{height:'250px' ,paddingBottom:'0px',objectFit: 'cover', opacity:'1',background:"rgba(0,0,0,1)",  borderRadius:'15px'}} alt=""></img>
                                 </div>
