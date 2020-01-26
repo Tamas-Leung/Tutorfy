aea01e1a85792ceb4ed446abc301df8a809f89ed
Madhi <madhi.naga@gmail.com>
2020-01-26 05:24:36 -0500

    Fixed site descriptions

--- src/Components/AddTutor.jsx
+++ src/Components/AddTutor.jsx
@@ -95,8 +95,8 @@ export class AddTutor extends Component {
             
 
       render() {
-        if (this.state.show) document.title = "Price Watch | Add Product";
-        else document.title = "Price Watch"
+        if (this.state.show) document.title = "Tutorly | Find a Local Tutor";
+        else document.title = "Tutorly"
           return (
             <div >
             <Dialog open={this.state.show} aria-labelledby="form-dialog-title">
@@ -147,7 +147,7 @@ export class AddTutor extends Component {
                 </DialogActions>
             </Dialog>
             <Button variant='contained' style={{marginTop:"0px", borderRadius:'100px', background:'#5185ed', fontFamily:'Avenir, Nunito Sans, sans-serif', fontWeight:'900', textTransform:'none', fontSize:'23px', padding:'0px 25px 0px 25px', color:'white', outline:'0'}} 
-            onClick={() => this.setState({show:true, added: false, error: false, emailValid: true})}>Add Tutor</Button>
+            onClick={() => this.setState({show:true, added: false, error: false, emailValid: true})}>Add a Post</Button>
             <Snackbar
                 style={{fontFamily:'Avenir, Nunito Sans, sans-serif', fontWeight:'900'}}
                 anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}
