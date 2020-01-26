48f7dcaf21dda5218770baf43c8500e9f6efe677
Tamas Leung <tamasleungwork@gmail.com>
2020-01-26 01:39:28 -0500

    result

--- src/Components/ResultCard.jsx
+++ src/Components/ResultCard.jsx
@@ -4,16 +4,22 @@ export class ResultCard extends Component {
     render() {
         var tutor = this.props.tutor
         return (
-            <div className="col-8 container" style={{marginBottom:"25px", background:"white"}}>
+            <div className="col-8 container" style={{marginBottom:"25px", background:"white", borderRadius:'15px'}}>
                 <div className="row" style={{fontFamily:'Avenir, sans-serif'}}>
                     <div className="col-3">
                         <div style={{height:'24 0px'}}>
                             <img className="resultCardImg" src={tutor.image} style={{height:'240px' ,paddingBottom:'0px',objectFit: 'cover', opacity:'1',background:"rgba(0,0,0,1)", borderRadius:'15px'}} alt=""></img>
                         </div>
                     </div>
-                    <div className="col-9" style={{fontSize:'27px', fontWeight:'900'}}>
-                        {tutor.courseName}
+                    <div className="col-9 row" style={{padding:'0px'}}>
+                        <div className="col-9" style={{fontSize:'27px', fontWeight:'900'}}>
+                            {tutor.courseName}
+                        </div>
+                        <div className="col-3" style={{color: "#5ba2ff", fontSize:'27px', fontWeight:'100', float:"right"}}>
+                            ${tutor.price}/hr
+                        </div>
                     </div>
+                    
                 </div>
                
             </div>
