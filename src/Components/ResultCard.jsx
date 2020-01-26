76efb62593d0dc72788decb4c39e6cc5edc5986b
Tamas Leung <tamasleungwork@gmail.com>
2020-01-26 01:00:12 -0500

    changed index and resultcard

--- src/Components/ResultCard.jsx
+++ src/Components/ResultCard.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react'
 
 export class ResultCard extends Component {
     render() {
+        var tutor = this.props.tutor
         return (
-            <div>
-               {this.props.tutor.courseName} 
+            <div className="col-8 container" style={{marginBottom:"25px", background:"white"}}>
+                <div className="row">
+                    <div className="col-4">
+                        <div style={{height:'240px'}}>
+                            <img className="resultCardImg" src={tutor.image} style={{height:'240px' ,paddingBottom:'0px',objectFit: 'cover', opacity:'1',background:"rgba(0,0,0,1)", borderRadius:'15px'}} alt=""></img>
+                        </div>
+                    </div>
+                    <div className="col-5">
+                        {tutor.courseName}
+                    </div>
+                    <div className="col-3">
+                        {tutor.price}
+                    </div>
+                </div>
+               
             </div>
         )
     }
