ac01a64c1daa7b267049aafc2f68085c1aedb2a3
Anton Kanugalawattage <antondilon@gmail.com>
2020-01-26 01:06:26 -0500

    minor update

--- src/Components/ResultCard.jsx
+++ src/Components/ResultCard.jsx
@@ -5,17 +5,14 @@ export class ResultCard extends Component {
         var tutor = this.props.tutor
         return (
             <div className="col-8 container" style={{marginBottom:"25px", background:"white"}}>
-                <div className="row">
-                    <div className="col-4">
-                        <div style={{height:'240px'}}>
+                <div className="row" style={{fontFamily:'Avenir, sans-serif'}}>
+                    <div className="col-3">
+                        <div style={{height:'24 0px'}}>
                             <img className="resultCardImg" src={tutor.image} style={{height:'240px' ,paddingBottom:'0px',objectFit: 'cover', opacity:'1',background:"rgba(0,0,0,1)", borderRadius:'15px'}} alt=""></img>
                         </div>
                     </div>
-                    <div className="col-5">
+                    <div className="col-9" style={{fontSize:'27px', fontWeight:'900'}}>
                         {tutor.courseName}
-                    </div>
-                    <div className="col-3">
-                        {tutor.price}
                     </div>
                 </div>
                
